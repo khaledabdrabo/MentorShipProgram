51c8a1143257a35efee5b6e116d8a75f7199fcc8
khaled abdrabo <khaledyousef434@gmail.com>
2024-02-28 18:41:39 +0200

    Add project files.

---
+++ Mentorship.Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mentorship.Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
